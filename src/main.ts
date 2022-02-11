2a177e1aee2a20aa5278e4682ca4174ccd21e7f3
hyejin-1010 <hyepago@gmail.com>
2022-02-11 23:48:41 +0900

    Fix: router Bug

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,13 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
+import router from "./router";
+import { createPinia } from "pinia";
 
 loadFonts();
 
-createApp(App).use(vuetify).mount("#app");
+const app = createApp(App);
+app.use(createPinia());
+app.use(vuetify);
+app.use(router);
+app.mount("#app");
