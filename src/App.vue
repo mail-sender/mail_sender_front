360afe87418ed67eaebde7080e3856853679043f
hyejin-1010 <hyepago@gmail.com>
2022-02-13 23:33:31 +0900

    Add: Navbar & SideMenu

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,15 @@
 <template>
-  <!-- TODO: AppBar -->
-  <!-- TODO: SideBar -->
   <div>
-    <n-button type="primary">naive-ui</n-button>
-    <RouterView />
+    <div class="appbar"></div>
+    <div class="content">
+      <div class="menu">
+        <n-menu :options="menuOptions" />
+      </div>
+      <div class="container">
+        <RouterView />
+      </div>
+    </div>
   </div>
-  <!--
-  <v-app>
-    <v-main>
-      
-      <v-container>
-        
-        
-      </v-container>
-    </v-main>
-  </v-app>
-  -->
 </template>
 
 <script>
@@ -24,12 +18,48 @@ import { RouterView } from "vue-router";
 export default {
   name: "App",
   components: { RouterView },
-  data: () => ({
-    //
-  }),
+  data: () => ({}),
+  setup() {
+    return { menuOptions: menus };
+  },
 };
+
+const menus = [
+  { label: "ACCOUNT", key: "account" },
+  {
+    label: "BODY FORMAT",
+    key: "bodyFormat",
+    children: [
+      {
+        label: "FORMAT 01",
+        key: "format_01",
+      },
+      {
+        label: "FORMAT 02",
+        key: "format_02",
+      },
+    ],
+  },
+  {
+    label: "CONTACT LIST",
+    key: "contactList",
+    children: [],
+  },
+];
 </script>
 
-<style>
+<style lang="scss">
 @import "@/assets/base.css";
+
+.appbar {
+  height: 50px;
+  background-color: var(--point-color);
+}
+
+.content {
+  display: flex;
+  .menu {
+    width: 300px;
+  }
+}
 </style>
