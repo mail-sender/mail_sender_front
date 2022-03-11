33a4d6e18caa56a1ef5819f1d3e3dafedb03d502
hyejin-1010 <hyepago@gmail.com>
2022-03-11 21:48:28 +0900

    Feat: ContactForm

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -6,10 +6,17 @@
           v-for="(contact, index) in contacts"
           :key="`contact-item-${index}`"
         >
-          <ContactItem :contact="contact" />
+          <ContactItem :contact="contact"
+            @edit="clickEditContactButton(contact, index)" />
         </div>
       </div>
     </n-card>
+
+    <n-modal v-model:show="showContactModal" transform-origin="center">
+      <n-card style="width: 550px" role="dialog" aria-modal="true">
+        <ContactForm :contactData="modalData.contact" />
+      </n-card>
+    </n-modal>
   </main>
 </template>
 
@@ -17,17 +24,31 @@
 import { useDataStore } from "@/stores/data";
 import type { Contact } from "@/models/contact.interface";
 import ContactItem from "@/components/ContactItem.vue";
+import ContactForm from "@/components/ContactForm.vue";
 
 export default {
-  components: { ContactItem },
+  components: { ContactItem, ContactForm },
   setup() {
     return {
       data: useDataStore(),
     };
   },
+  data() {
+    return {
+      modalData: { contact: null, index: null },
+      showContactModal: false,
+    };
+  },
   computed: {
     contacts(): Array<Contact> {
       return this.data.contacts;
+    },
+  },
+  methods: {
+    clickEditContactButton(contact: Contact, index: number) {
+      contact = JSON.parse(JSON.stringify(contact)) as Contact;
+      this.modalData = { contact, index };
+      this.showContactModal = true;
     },
   },
 };
